3d2b2d32a64dfd62ca3afb88c3d9a769389c6cf0
EllytptiumOfficial <fosanzgames@gmail.com>
2020-04-07 21:26:36 +0200

    diagonal provisional save

--- diagonal.py
+++ diagonal.py
@@ -1,1 +1,11 @@
-print("Acceleration, yes")
+
+def diagonal(array, side_1, side_2, reverse=False):
+    if reverse == True:
+        for a in array:
+            array.append(a[::-1])
+            
+        array = array[::-1]
+    
+    print(array)
+
+diagonal([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1, 2, True)
